4d323cb089a199716549cb38de26445dcd57d4d5
AladinBrapo <jayram.celinscak@scv.si>
2025-02-12 10:22:45 +0100

    fixed login and categories

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,7 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_one_attached :image
 
+  validates :category, presence: true
   validates :title, :description, :price, presence: true
 end
 
