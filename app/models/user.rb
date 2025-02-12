4f3c894a51b85be15ac3cd4e0e1f30bcb6f469e4
AladinBrapo <jayram.celinscak@scv.si>
2025-02-12 09:42:54 +0100

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :listings, dependent: :destroy  # A user can have many listings, and if the user is deleted, their listings will be deleted as well.
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
