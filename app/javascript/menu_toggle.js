82692feb747f1620ead6d76c869c8adbac49271f
AladinBrapo <jayram.celinscak@scv.si>
2025-03-05 10:13:21 +0100

    header fixed

--- app/javascript/menu_toggle.js
+++ app/javascript/menu_toggle.js
@@ -1,6 +1,12 @@
 export function toggleMenu() {
     console.log("Menu toggled!");
-    document.querySelector(".nav-left .nav-links li").classList.toggle("active");
-    document.querySelector(".nav-right").classList.toggle("active");
-    document.querySelector(".nav-middle").classList.toggle("active");
-}
+    const navLeft = document.querySelector(".nav-left .nav-links");
+    const navMiddle = document.querySelector(".nav-middle");
+    const navRight = document.querySelector(".nav-right");
+  
+    // Toggle 'active' class on the nav-left, nav-middle, and nav-right
+    navLeft.classList.toggle("active");
+    navMiddle.classList.toggle("active");
+    navRight.classList.toggle("active");
+  }
+  
