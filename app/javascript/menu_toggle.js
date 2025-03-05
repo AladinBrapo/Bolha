fd45c557f618278b59085ab823b7513b82903415
AladinBrapo <jayram.celinscak@scv.si>
2025-03-05 09:23:36 +0100

    custom.css

--- app/javascript/menu_toggle.js
+++ app/javascript/menu_toggle.js
@@ -1,6 +1,6 @@
 export function toggleMenu() {
     console.log("Menu toggled!");
-    document.querySelector(".nav-left .nav-links").classList.toggle("active");
+    document.querySelector(".nav-left .nav-links li").classList.toggle("active");
     document.querySelector(".nav-right").classList.toggle("active");
     document.querySelector(".nav-middle").classList.toggle("active");
 }
