8f8ba2ecf83edebecde9fdf27d98f18a636cde97
AladinBrapo <jayram.celinscak@scv.si>
2025-03-09 13:32:20 +0100

    small header fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :authenticate_user!
+
   def after_sign_in_path_for(resource)
-    listings_path # Redirect to the listings index page, for example
+    if resource.admin?
+      admin_root_path # Redirects to `/admin`
+    else
+      root_path # Normal users go to the homepage
+    end
   end
 end
