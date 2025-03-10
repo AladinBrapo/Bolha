699b3624da012ddb87100592d5825a68d060bbf2
AladinBrapo <jayram.celinscak@scv.si>
2025-03-10 17:47:52 +0100

    domain and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
+
+
   def after_sign_in_path_for(resource)
     if resource.admin?
       admin_root_path # Redirects to `/admin`
