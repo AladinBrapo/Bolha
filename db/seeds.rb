d8d7b221c36b3243eb0b3e221d83140feaeb5ff2
AladinBrapo <jayram.celinscak@scv.si>
2025-03-11 09:19:45 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,27 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+# Ensure that the admin account is created if it doesn't exist
+admin_email = 'jayram.celinscak@scv.si'
+admin_password = ENV['ADMIN_PASSWORD']  # Make sure to change this for production
+
+Admin.find_or_create_by!(email: admin_email) do |admin|
+  admin.password = admin_password
+  admin.password_confirmation = admin_password
+end
+
+categories = [
+  "Elektronika",
+  "Oblačila in obutev",
+  "Pohištvo",
+  "Avtomobili in prevozna sredstva",
+  "Otroci in igrače"
+]
+
+# Poskrbimo, da so kategorije ustvarjene, če še ne obstajajo
+categories.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
+
